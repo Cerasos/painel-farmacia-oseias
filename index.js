17a30f27d305e2dab48090c6c466757ed030cac1
Kaykyvinicius <viniciuskayky23@gmail.com>
2025-10-17 19:20:32 -0300

    fix: resolve import issue with paymentRoutes in Express app

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import webhookRoutes from "./routes/webhook.js";
 import apiRoutes from "./routes/api.js";
 import uazapiRoutes from "./routes/uazapi.js";
 import painelRoutes from "./routes/painel.js";
-import paymentRoutes from "./payments/routes/paymentRoutes.js";
+import * as paymentRoutes from "./payments/routes/paymentRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/webhook", webhookRoutes);
 app.use("/api", apiRoutes);
 app.use("/uazapi", uazapiRoutes);
-app.use("/payments", paymentRoutes);
+app.use("/payments", paymentRoutes.default);
 app.use("/", painelRoutes);
 
 export default app;
