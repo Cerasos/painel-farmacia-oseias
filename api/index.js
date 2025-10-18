d5309bcab6677946fb4bb0ae750af53a33994b5c
Kaykyvinicius <viniciuskayky23@gmail.com>
2025-10-18 01:03:08 -0300

    feat: added railway public url for index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,14 @@
 import express from "express";
+import serverless from "serverless-http";
 
 import webhookRoutes from "../routes/webhook.js";
 import apiRoutes from "../routes/api.js";
 import uazapiRoutes from "../routes/uazapi.js";
 import painelRoutes from "../routes/painel.js";
 import * as paymentRoutes from "../payments/routes/paymentRoutes.js";
+
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 app.use(express.json());
@@ -15,7 +19,20 @@ app.use("/uazapi", uazapiRoutes);
 app.use("/payments", paymentRoutes.default);
 app.use("/", painelRoutes);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const publicPath = path.join(__dirname, "..", "public");
+
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(publicPath, "painel.html"));
+});
+
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+
+export default serverless(app);
