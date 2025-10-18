55924cf1b818f1990a3cea3ba64c849796bfe26a
Kaykyvinicius <viniciuskayky23@gmail.com>
2025-10-18 01:19:12 -0300

    fix: index.js

--- api/index.js
+++ api/index.js
@@ -8,15 +8,15 @@ import * as paymentRoutes from "../payments/routes/paymentRoutes.js";
 const app = express();
 app.use(express.json());
 
+app.use(express.static("../public"));
+
 app.use("/webhook", webhookRoutes);
 app.use("/api", apiRoutes);
 app.use("/uazapi", uazapiRoutes);
 app.use("/payments", paymentRoutes.default);
 app.use("/", painelRoutes);
 
-app.use(express.static("public"));
-
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
