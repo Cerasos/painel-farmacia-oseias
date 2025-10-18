918014026d891127eae1d96f9f3a55f363d5bf03
Kaykyvinicius <viniciuskayky23@gmail.com>
2025-10-17 23:46:49 -0300

    chore: remove duplicate index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
+import express from "express";
 import serverless from "serverless-http";
-import app from "../index.js";
-import app from "../../api.js";
 
-export const handler = serverless(app);
+import webhookRoutes from "../routes/webhook.js";
+import apiRoutes from "../routes/api.js";
+import uazapiRoutes from "../routes/uazapi.js";
+import painelRoutes from "../routes/painel.js";
+import * as paymentRoutes from "../payments/routes/paymentRoutes.js";
+
+const app = express();
+app.use(express.json());
+
+app.use("/webhook", webhookRoutes);
+app.use("/api", apiRoutes);
+app.use("/uazapi", uazapiRoutes);
+app.use("/payments", paymentRoutes.default);
+app.use("/", painelRoutes);
+
+export default serverless(app);
